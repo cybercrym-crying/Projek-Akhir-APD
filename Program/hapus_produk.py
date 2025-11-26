d752a9978f2ec867276a08b1e8eb80cb59f8e39a
bero <crymbero@gmail.com>
2025-11-26 20:26:38 +0800

    fix error hapus keranjang dan hapus produk

--- Program/hapus_produk.py
+++ Program/hapus_produk.py
@@ -7,17 +7,18 @@ import variabel_global as var
 def hapusProduk():
 
     listProduk()
-    pilihan_id = int(qs.text("Masukan Id Barang Yang Ingin Di Hapus : ").ask())
-    if pilihan_id in var.daftar_barang.keys():
-        salinan_daftar_barang = deepcopy(var.daftar_barang)
-        key_akhir = len(var.daftar_barang.keys())
-        for batas in range(pilihan_id, key_akhir):
-            if pilihan_id < key_akhir:
-                salinan_daftar_barang[batas] = salinan_daftar_barang[batas + 1]
-                continue
-        del salinan_daftar_barang[key_akhir]
-        print("\n+ Hapus Barang Berhasil!\n")
-        var.daftar_barang = deepcopy(salinan_daftar_barang)
-    else:
+    try:
+        pilihan_id = int(qs.text("Masukan Id Barang Yang Ingin Di Hapus : ").ask())
+        if pilihan_id in var.daftar_barang.keys():
+            salinan_daftar_barang = deepcopy(var.daftar_barang)
+            key_akhir = len(var.daftar_barang.keys())
+            for batas in range(pilihan_id, key_akhir):
+                if pilihan_id < key_akhir:
+                    salinan_daftar_barang[batas] = salinan_daftar_barang[batas + 1]
+                    continue
+            del salinan_daftar_barang[key_akhir]
+            print("\n+ Hapus Barang Berhasil!\n")
+            var.daftar_barang = deepcopy(salinan_daftar_barang)
+    except ValueError:
         print("\n!! Pilihan Tidak Valid !!\n")
         return
